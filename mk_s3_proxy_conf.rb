b129ccd55896042ffcc3c7d25f94a157e6ec4526
Robert Iannucci <robbie@mixbook.com>
2012-05-30 15:13:08 -0700

    Improve documentation slightly

--- mk_s3_proxy_conf.rb
+++ mk_s3_proxy_conf.rb
@@ -4,14 +4,22 @@ require 'erubis'
 # Use like:
 #  mk_s3_proxy_conf.rb << EOF
 #  product_bucket_name production_aws_id production_key
-#  staging_bucket_name staging_aws_id staging_aws_key
-#  dev1_bucket_name dev1_aws_id dev1_aws_key
+#  staging_bucket_name
+#  dev1_bucket_name
 #  EOF
 #
 # You may have as many staging/dev/test buckets as you like.
 #
 # Queries must be issued to the proxy with a host in the form of:
 #   staging.s3.amazonaws.com
+#   dev.s3.amazonaws.com
+#
+# etc. This means that PUT operations will always pass through without any
+# resigning necessary.
+#
+# For goodness, you should issue a read-only key for production. This is
+# because you don't want the proxy to ever 'write' to the production bucket.
+#
 #
 
 buckets = STDIN.readlines.map(&:chomp)
