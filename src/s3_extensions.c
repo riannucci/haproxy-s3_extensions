b79a76e6671fa5c31a706653b7769ea1a07bc2db
Robert Iannucci <robbie@mixbook.com>
2012-06-01 04:52:16 -0700

    Take initial stab at implementing the redis bits.

--- src/s3_extensions.c
+++ src/s3_extensions.c
@@ -4,15 +4,57 @@
 #include <openssl/hmac.h>
 #include <hiredis/hiredis.h>
 
+static redisContext *g_redis_ctxt = NULL;
+
+redisReply *doRedisCommand(const char *fmt, ...) {
+  va_list ap;
+  int tried = 0;
+
+retry:
+  if(!g_redis_ctxt) {
+    // TODO: Allow setting server/port
+    g_redis_ctxt = redisConnect("127.0.0.1", 6379);
+  }
+
+  va_start(ap,fmt);
+  redisReply *reply = redisvCommand(g_redis_ctxt, fmt, ap);
+  va_end(ap);
+
+  if(!reply) {
+    if(!tried && g_redis_ctxt->err == REDIS_ERR_EOF) {
+      // Connection probably timed out
+      tried = 1;
+      redisFree(g_redis_ctxt);
+      g_redis_ctxt = NULL;
+      goto retry;
+    } else {
+      printf("Error: %s\n", g_redis_ctxt->errstr);
+    }
+  }
+
+  return reply;
+}
+
 int s3_already_redirected(struct acl_test *test, struct acl_pattern *pattern) {
-	// TODO: do redis here
-	return ACL_PAT_PASS;
+  const char *bucket      = pattern->ptr.str;
+  const int  object_len   = test->len;
+  const char *object_str  = test->ptr;
+
+  redisReply *reply = doRedisCommand("SISMEMBER %s %b", bucket, object_str, object_len);
+  int retval = !reply || reply->integer ? ACL_PAT_PASS : ACL_PAT_FAIL;
+
+  if(reply) freeReplyObject(reply);
+
+  return retval;
 }
 
 int s3_mark_redirected(struct acl_test *test, struct acl_pattern *pattern) {
   const char *bucket      = pattern->ptr.str;
   const int  object_len   = test->len;
   const char *object_str  = test->ptr;
+
+  redisReply *reply = doRedisCommand("SADD %s %b", bucket, object_str, object_len);
+  if(reply) freeReplyObject(reply);
 
   return ACL_PAT_PASS;
 }
