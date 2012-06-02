98533c47c4c4d9276835f6c6318252f44a44ae25
Robert Iannucci <robbie@mixbook.com>
2012-06-01 22:16:40 -0700

    Fix typo-bug in run_debian_build.sh

--- run_debian_build.sh
+++ run_debian_build.sh
@@ -2,4 +2,4 @@
 sudo apt-get update
 sudo apt-get install build-essential libpcre3-dev redis-server libssl-dev libboost-system-dev
 ( cd hiredis && make static )
-make USE_LINUX_SPLICE=1 TARGET=linux26 TARGET=native USE_STATIC_PCRE=1 
+make USE_LINUX_SPLICE=1 TARGET=linux26 CPU=native USE_STATIC_PCRE=1
