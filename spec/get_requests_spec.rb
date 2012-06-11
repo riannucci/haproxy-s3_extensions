3960c49c2826f33efaaedf9951a9688299415d5c
Robert Iannucci <robbie@rail.com>
2012-06-11 09:07:48 -0700

    Add a test to make sure odd keys work

--- spec/get_requests_spec.rb
+++ spec/get_requests_spec.rb
@@ -9,4 +9,16 @@ describe "GET operations" do
     expect { test_object(:master).write(test_data) }.not_to raise_error
     test_object.read.should == test_data
   end
+
+  it "should let you have keys with spaces, etc." do
+    filename = "This is a test/thing with spaces"
+    redis_set.should_not include(filename)
+    test_object(:obj => filename).should_not exist
+
+    test_data = "This is a test file!"
+    expect { test_object(:master, :obj=>filename).write(test_data) }.not_to raise_error
+
+    test_object(:obj => filename).read.should == test_data 
+    test_object(:obj => filename.split('/')[0]).should_not exist
+  end
 end
